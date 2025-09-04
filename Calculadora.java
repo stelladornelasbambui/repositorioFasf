26b6f8c00953dec2c1728110d2b2111147900023
stelladornelasbambui <140439856+stelladornelasbambui@users.noreply.github.com>
2025-09-04 20:15:40 -0300

    Add files via upload
    exemplo java

---
+++ Calculadora.java
@@ -0,0 +1,19 @@
+
+public class Calculadora {
+	public double somar(double num1, double num2) {
+		return num1+num2;
+		
+	}
+	
+	public double subtracao(double num1, double num2) {
+		return num1-num2;
+		
+	}
+	
+	public boolean validarNome( String nome) {
+		return nome.matches("^[\\p{L}+$");
+	}
+	
+	
+
+}
